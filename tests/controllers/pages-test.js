bf74742a552528c9117ec78affd98df34fef54a2
Nikita Pestov <grizzlyarchi@gmail.com>
2016-03-13 20:36:55 +0500

    style fix semicolon

--- tests/controllers/pages-test.js
+++ tests/controllers/pages-test.js
@@ -7,6 +7,6 @@ describe('Pages Controller', () => {
     it('Should respond with 404 on no existing page', done => {
         supertest(app)
             .get('/nonExistentPage')
-            .expect(404, done)
+            .expect(404, done);
     });
 });
