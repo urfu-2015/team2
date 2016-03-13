3267614d6f38214b233a5ddfa6c30a105c102129
Natalia Aksenova <dotokoto@gmail.com>
2016-03-13 15:16:45 +0500

    codestyle

--- tests/test.js
+++ tests/test.js
@@ -3,8 +3,8 @@
 const should = require('chai').should();
 
 
-describe('test for test', function() {
-    it('should return true', function() {
+describe('test for test', function () {
+    it('should return true', function () {
         var test = true;
         test.should.equal(true);
     });
