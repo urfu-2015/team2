3d40319e9720c968294da601a9468ff8bc78bf95
Nikita Pestov <grizzlyarchi@gmail.com>
2016-03-19 19:58:11 +0500

    added auth.js for login and logout

--- controllers/pages.js
+++ controllers/pages.js
@@ -10,15 +10,4 @@ exports.index = (req, res) => {
     res.render('main/main', Object.assign(data, req.commonData));
 };
 
-exports.logout = (req, res) => {
-    req.logout();
-    res.redirect('/');
-};
-
-exports.login = (req, res) => {
-    if (!req.user) {
-        throw new Error('user null');
-    }
-    res.redirect('/quests');
-};
 exports.error404 = (req, res) => res.sendStatus(404);
