026bcecb969aec63aedae151cae0dd021ce9ad07
Mokoshka <lonelygopnik@gmail.com>
2016-03-25 21:24:47 +0500

    Маленькая правка

--- middlewares/setup-passport.js
+++ middlewares/setup-passport.js
@@ -2,7 +2,6 @@ const passport = require('passport');
 const Auth0Strategy = require('passport-auth0');
 const settings = require('../settings.js');
 const clientSecret = process.env.AUTH0_CLIENTSECRET;
-console.log(settings);
 const clientID = settings.auth_clientID;
 
 const strategy = new Auth0Strategy({
