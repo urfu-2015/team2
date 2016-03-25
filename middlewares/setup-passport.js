b7c5a4f91f35229b291f450ae915fa417bb9994f
Mokoshka <lonelygopnik@gmail.com>
2016-03-25 21:22:17 +0500

    Замена файла настроек

--- middlewares/setup-passport.js
+++ middlewares/setup-passport.js
@@ -1,8 +1,9 @@
 const passport = require('passport');
 const Auth0Strategy = require('passport-auth0');
-const fs = require('fs');
+const settings = require('../settings.js');
 const clientSecret = process.env.AUTH0_CLIENTSECRET;
-const clientID = JSON.parse(fs.readFileSync('./settings')).auth_clientID;
+console.log(settings);
+const clientID = settings.auth_clientID;
 
 const strategy = new Auth0Strategy({
     domain: 'yahackathonteam2.auth0.com',
