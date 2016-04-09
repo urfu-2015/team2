4cd3dfb23959d5fd66d639bea004311e04c2ac18
Латыпова Екатерина < >
2016-04-09 19:23:30 +0500

    да удались уже наконец

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const Promise = require('bluebird');
 
 const express = require('express');
-const mongoose = require('mongoose');
 const url = require('url');
 const app = express();
 const listen = Promise.promisify(app.listen, { context: app });
