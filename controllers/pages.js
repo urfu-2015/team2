395d0541c49db8a545087f79be15a3c590975b15
Nikita Pestov <grizzlyarchi@gmail.com>
2016-04-09 23:38:15 +0500

    handlebars-layouts added

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,13 +1,28 @@
+const fs = require('fs');
+const layouts = require('handlebars-layouts');
+
+// Так нужно сделать, так как внутри handlebars лежит внутренний нужный метод
+const hbs = require('hbs');
+const handlebars = hbs.handlebars;
+
+// Register helpers
+handlebars.registerHelper(layouts(handlebars));
+
+// Register partials
+handlebars.registerPartial('base', fs.readFileSync('./bundles/base.hbs', 'utf8'));
+
 exports.quests = (req, res) => {
-    res.render('main/main', req.commonData);
+    const template = handlebars.compile(fs.readFileSync('./bundles/main/main.hbs', 'utf8'));
+    res.send(template(req.commonData));
 
 };
 
 exports.index = (req, res) => {
+    const template = handlebars.compile(fs.readFileSync('./bundles/main/main.hbs', 'utf8'));
     const data = {
         currentCity: 'Екатеринбург'
     };
-    res.render('main/main', Object.assign(data, req.commonData));
+    res.send(template(Object.assign(data, req.commonData)));
 };
 
 exports.error404 = (req, res) => res.sendStatus(404);
