7694863d4a445466fbbc60b116855cbf1e459505
Латыпова Екатерина <katelat3009@gmail.com>
2016-05-01 14:29:24 +0500

    Локальная БД

--- scripts/mongooseConnect.js
+++ scripts/mongooseConnect.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 const config = require('config');
-const dbUrl = process.env.DB_CONNECT || config.get('db.mongodb_URL');
+const dbUrl = process.env.DB_CONNECT || config.get('db.local');
 
 mongoose.connect(dbUrl);
 mongoose.connection.on('error', console.error.bind(console, 'connection error'));
