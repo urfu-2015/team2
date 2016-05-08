5b8900cbc5cadba37297fb8b355387590936d0d2
Латыпова Екатерина <katelat3009@gmail.com>
2016-05-08 16:22:08 +0500

    подключание к бд

--- scripts/mongooseConnect.js
+++ scripts/mongooseConnect.js
@@ -4,10 +4,15 @@ const mongoose = require('mongoose');
 const config = require('config');
 
 const argv = require('minimist')(process.argv.slice(4));
+let dbUrl;
 
-const dbUrl = process.env.DB_CONNECT || (argv.STATUS === 'testing') ?
-    config.get('db.local') : config.get('db.mongodb_URL');
-console.log(dbUrl);
+if (process.env.DB_CONNECT) {
+    dbUrl = process.env.DB_CONNECT;
+} else if (argv.STATUS === 'testing') {
+    dbUrl = config.get('db.local');
+} else {
+    dbUrl = config.get('db.mongodb_URL');
+}
 
 mongoose.connect(dbUrl);
 mongoose.connection.on('error', console.error.bind(console, 'connection error'));
