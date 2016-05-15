7ff27fbe64cd28b721c9318d8c7ff52d7017b8cb
Vasilii <kuznecov_vasilii@.mail.ru>
2016-05-15 13:20:19 +0500

    init quest form

--- blocks/questForm/questForm.js
+++ blocks/questForm/questForm.js
@@ -1,9 +1,17 @@
 require('./questForm.css');
-/*
 require('../add-stage/add-stage.js');
 const template = require('../add-stage/add-stage.hbs');
-*/
 
 $('.quest-form__new-stage-button').click(function (event) {
     $('.quest-form__stages').append($.parseHTML(template()));
+});
+
+$('.quest-form__stages').append($.parseHTML(template()));
+
+$('.quest-form__submit').click(function (event) {
+    var questData = {
+        name: $('.quest-form__name-input').value,
+        city: $('.quest-form__city-input').value,
+        description: $('.quest-form__description-input').value
+    };
 });
