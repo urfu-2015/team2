7c80eaa0d57b408b7c2ea1d565ee31b7ed8ed6cf
Nikita Pestov <grizzlyarchi@gmail.com>
2016-05-20 01:04:18 +0500

    huge commit quests page and checkins

--- blocks/quest_page_controls/quest_page_controls.js
+++ blocks/quest_page_controls/quest_page_controls.js
@@ -1,1 +1,50 @@
 require('./quest_page_controls.css');
+
+
+$(document).ready(() => {
+    $('button.begin').on('click',() => {
+        let questId = $('#questId').val();
+        const data = {
+            questId: questId
+        };
+
+        const checkinModal = $('#checkin-modal');
+        checkinModal.modal(); // Активируем модальное окно
+        checkinModal.modal('show');
+        checkinModal.scrollTop(0);
+
+        checkinModal.on('hidden.bs.modal', function (e) {
+            $(this).find('.modal-body p').text('Подождите пожалуйста...');
+        });
+        checkinModal.on('hide.bs.modal', function (e) {
+            location.reload();
+        });
+
+        $.ajax({
+            url: '/quests/start',
+            type: 'POST',
+            data: data
+        }).done(result => {
+            console.log(result);
+            let modalBody = checkinModal.find('.modal-body p');
+            let text = 'Поздравляем, вы успешно начали новый квест!';
+            modalBody.text(text);
+        }).fail(err => {
+            console.log(err);
+            let modalBody = checkinModal.find('.modal-body p');
+            let text;
+            switch (err.status) {
+                case 401:
+                    text = 'Извините, но начинать проходить квест могут только зарегистрированные пользователи..';
+                    break;
+                case 400:
+                    text = 'Извините, но вы уже начали проходить данный квест.';
+                    break;
+                default:
+                    text = 'Извините, произошла внутренняя ошибка сервиса, попробуйте еще раз.';
+            }
+            modalBody.text(text);
+        });
+    })
+
+});
--- blocks/stages/stages.js
+++ blocks/stages/stages.js
@@ -1,1 +1,71 @@
-require('./stages.css');
+const geolib = require('geolib');
+
+if (!navigator.geolocation) {
+    showError({ text: 'Ваш браузер не поддерживает геолокацию' });
+} else {
+    $('button.checkin').on('click', function () {
+        const form = $(this).parents('.stage-checkin');
+        const stageId = form.find('[name="stageID"]').val() || 1;
+        const questId = form.find('[name="questID"]').val() || 1;
+
+        const checkinModal = $('#checkin-modal');
+        checkinModal.modal(); // Активируем модальное окно
+        checkinModal.modal('show');
+        checkinModal.scrollTop(0);
+
+        checkinModal.on('hidden.bs.modal', function (e) {
+            $(this).find('.modal-body p').text('Подождите пожалуйста...');
+        });
+        checkinModal.on('hide.bs.modal', function (e) {
+            location.reload();
+        });
+
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                let latitude = position.coords.latitude;
+                let longitude = position.coords.longitude;
+                const data = {
+                    questId: questId,
+                    stageId: stageId,
+                    latitude,
+                    longitude
+                };
+                $.ajax({
+                    url: '/stages/checkins',
+                    type: 'POST',
+                    data: data
+                }).done(function (result) {
+                    console.log(result);
+                    let modalBody = checkinModal.find('.modal-body p');
+                    let text;
+                    switch (result.checkin) {
+                        case true:
+                            text = 'Поздравляем! Вы успешно прошли этап квеста!';
+                            break;
+                        default:
+                            text = 'Извините, но вы вне радиуса принятия отметки. Попробуйте подойти ближе';
+                    }
+                    modalBody.text(text);
+                }).fail(function (err) {
+                    let modalBody = checkinModal.find('.modal-body p');
+                    let text;
+                    switch (err.status) {
+                        case 401:
+                            text = 'Извините, но отмечать этап могут только авторизованные пользователи.';
+                            break;
+                        default:
+                            text = 'Извините, произошла внутренняя ошибка сервиса, попробуйте еще раз';
+                    }
+                    modalBody.text(text);
+                });
+            },
+            function (err) {
+                let modalBody = checkinModal.find('.modal-body p');
+                modalBody.text('Извините, мы не смогли получить данные о местоположении');
+            },
+            {
+                enableHighAccuracy: true
+            }
+        );
+    });
+}
